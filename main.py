429eeec62e268f2c30aa8c451cd6cb77320a3221
vitoraugustx <vitor.arp2010@gmail.com>
2023-05-04 13:45:10 -0300

    wip

--- main.py
+++ main.py
@@ -6,60 +6,83 @@ from sklearn.inspection import DecisionBoundaryDisplay
 
 from sklearn.model_selection import train_test_split
 
+def plot_graph(income_x,income_y, fig_title):
+
+    global models
+    global choosenCols
+
+    # title for the plots
+    titles = (
+        "SVM com kernel linear",
+        "SVM com kernel radial",
+        "SVM com kernel polinomial (grau 3)",
+    )
+
+    # Set-up 2x2 grid for plotting.
+    fig, sub = plt.subplots(nrows=2, ncols=2)
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+    X0, X1 = income_x[:, 0], income_x[:, 1]
+
+    for clf, title, ax in zip(models, titles, sub.flatten()):
+        disp = DecisionBoundaryDisplay.from_estimator(
+            clf,
+            income_x,
+            response_method="predict",
+            cmap=plt.cm.coolwarm,
+            alpha=0.8,
+            ax=ax,
+            xlabel=choosenCols[0],
+            ylabel=choosenCols[1],
+        )
+        ax.scatter(X0, X1, c=income_y, cmap=plt.cm.coolwarm, s=20, edgecolors="k")
+        ax.set_xticks(())
+        ax.set_yticks(())
+        ax.set_title(title)
+    fig.canvas.manager.set_window_title(fig_title)
+    plt.show()
+
 df = pd.read_csv('Heart.csv')
 df.drop(['Unnamed: 0'], axis=1, inplace=True)
 
-x = df[['RestBP','Chol']]
+# drop NaN
+df = df.dropna()
+
+cols = ['ChestPain','Thal', 'AHD']
+
+df[cols] = df[cols].apply(lambda x: pd.factorize(x)[0])
+
+print(df.head())
+
+choosenCols = ['MaxHR','Age']
+
+x = df[choosenCols]
 y = df['AHD']
-y = pd.factorize(y)[0]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
+
+
 # we create 40 separable points
-X, y = x_train.to_numpy(), y_train
+X, y = x_train.to_numpy(), y_train.to_numpy()
 
 # we create an instance of SVM and fit out data. We do not scale our
 # data since we want to plot the support vectors
-C = 0.00001  # SVM regularization parameter
+C = 0.0001  # SVM regularization parameter
 models = (
     svm.SVC(kernel="linear", C=C),
-    svm.LinearSVC(C=C, max_iter=10000),
-    svm.SVC(kernel="rbf", gamma=0.7, C=C),
+    svm.SVC(kernel="rbf", gamma=0.01),
     svm.SVC(kernel="poly", degree=3, gamma="auto", C=C),
 )
 models = (clf.fit(X, y) for clf in models)
 
-# title for the plots
-titles = (
-    "SVC with linear kernel",
-    "LinearSVC (linear kernel)",
-    "SVC with RBF kernel",
-    "SVC with polynomial (degree 3) kernel",
-)
+#plot_graph(X,y,'Treinamento')
+plot_graph(x_test.to_numpy(),y_test,'Teste')
 
-# Set-up 2x2 grid for plotting.
-fig, sub = plt.subplots(2, 2)
-plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-X0, X1 = X[:, 0], X[:, 1]
 
-for clf, title, ax in zip(models, titles, sub.flatten()):
-    disp = DecisionBoundaryDisplay.from_estimator(
-        clf,
-        X,
-        response_method="predict",
-        cmap=plt.cm.coolwarm,
-        alpha=0.8,
-        ax=ax,
-        xlabel='RestBP',
-        ylabel='Chol',
-    )
-    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors="k")
-    ax.set_xticks(())
-    ax.set_yticks(())
-    ax.set_title(title)
 
-plt.show()
+
 
 
 
--- test.py
+++ test.py
@@ -1,37 +1,55 @@
 import matplotlib.pyplot as plt
-from sklearn import svm
-from sklearn.datasets import make_blobs
+from sklearn import svm, datasets
 from sklearn.inspection import DecisionBoundaryDisplay
 
 
-# we create 40 separable points
-X, y = make_blobs(n_samples=40, centers=2, random_state=6)
+# import some data to play with
+iris = datasets.load_iris()
+# Take the first two features. We could avoid this by using a two-dim dataset
+X = iris.data[:, :2]
+y = iris.target
 
-# fit the model, don't regularize for illustration purposes
-clf = svm.SVC(kernel="linear", C=1000)
-clf.fit(X, y)
+print(y)
 
-plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
-
-# plot the decision function
-ax = plt.gca()
-DecisionBoundaryDisplay.from_estimator(
-    clf,
-    X,
-    plot_method="contour",
-    colors="k",
-    levels=[-1, 0, 1],
-    alpha=0.5,
-    linestyles=["--", "-", "--"],
-    ax=ax,
+# we create an instance of SVM and fit out data. We do not scale our
+# data since we want to plot the support vectors
+C = 1.0  # SVM regularization parameter
+models = (
+    svm.SVC(kernel="linear", C=C),
+    svm.LinearSVC(C=C, max_iter=10000),
+    svm.SVC(kernel="rbf", gamma=0.7, C=C),
+    svm.SVC(kernel="poly", degree=3, gamma="auto", C=C),
 )
-# plot support vectors
-ax.scatter(
-    clf.support_vectors_[:, 0],
-    clf.support_vectors_[:, 1],
-    s=100,
-    linewidth=1,
-    facecolors="none",
-    edgecolors="k",
+models = (clf.fit(X, y) for clf in models)
+
+# title for the plots
+titles = (
+    "SVC with linear kernel",
+    "LinearSVC (linear kernel)",
+    "SVC with RBF kernel",
+    "SVC with polynomial (degree 3) kernel",
 )
+
+# Set-up 2x2 grid for plotting.
+fig, sub = plt.subplots(2, 2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+X0, X1 = X[:, 0], X[:, 1]
+
+for clf, title, ax in zip(models, titles, sub.flatten()):
+    disp = DecisionBoundaryDisplay.from_estimator(
+        clf,
+        X,
+        response_method="predict",
+        cmap=plt.cm.coolwarm,
+        alpha=0.8,
+        ax=ax,
+        xlabel=iris.feature_names[0],
+        ylabel=iris.feature_names[1],
+    )
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors="k")
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(title)
+
 plt.show()
