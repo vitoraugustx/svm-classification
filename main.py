4f6eb2ca8f3adc6069b1a0cfc2cd1acd61bdee4f
Vitor <vitor.arp2010@gmail.com>
2023-05-03 22:10:20 -0300

    wip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn import svm
 import matplotlib.pyplot as plt
 from sklearn.inspection import DecisionBoundaryDisplay
 
@@ -10,55 +11,54 @@ df.drop(['Unnamed: 0'], axis=1, inplace=True)
 
 x = df[['RestBP','Chol']]
 y = df['AHD']
+y = pd.factorize(y)[0]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
-# Treina o modelo SVM com kernel linear, polinomial e radial
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+# we create 40 separable points
+X, y = x_train.to_numpy(), y_train
 
-# Kernel linear
-clf = SVC(kernel='linear')
-clf.fit(x_train, y_train)
-y_pred = clf.predict(x_test)
-print('Accuracy (linear):', accuracy_score(y_test, y_pred))
-
-# # Kernel polinomial
-# clf = SVC(kernel='poly')
-# clf.fit(x_train, y_train)
-# y_pred = clf.predict(x_test)
-# print('Accuracy (poly):', accuracy_score(y_test, y_pred))
-
-# # Kernel radial
-# clf = SVC(kernel='rbf')
-# clf.fit(x_train, y_train)
-# y_pred = clf.predict(x_test)
-# print('Accuracy (rbf):', accuracy_score(y_test, y_pred))
-
-# Plotar resultados usando matplotlib
-plt.scatter(x[:, 0], x[:, 1], c=y, s=30, cmap=plt.cm.Paired)
-
-# plot the decision function
-ax = plt.gca()
-DecisionBoundaryDisplay.from_estimator(
-    clf,
-    x,
-    plot_method="contour",
-    colors="k",
-    levels=[-1, 0, 1],
-    alpha=0.5,
-    linestyles=["--", "-", "--"],
-    ax=ax,
+# we create an instance of SVM and fit out data. We do not scale our
+# data since we want to plot the support vectors
+C = 0.00001  # SVM regularization parameter
+models = (
+    svm.SVC(kernel="linear", C=C),
+    svm.LinearSVC(C=C, max_iter=10000),
+    svm.SVC(kernel="rbf", gamma=0.7, C=C),
+    svm.SVC(kernel="poly", degree=3, gamma="auto", C=C),
 )
-# plot support vectors
-ax.scatter(
-    clf.support_vectors_[:, 0],
-    clf.support_vectors_[:, 1],
-    s=100,
-    linewidth=1,
-    facecolors="none",
-    edgecolors="k",
+models = (clf.fit(X, y) for clf in models)
+
+# title for the plots
+titles = (
+    "SVC with linear kernel",
+    "LinearSVC (linear kernel)",
+    "SVC with RBF kernel",
+    "SVC with polynomial (degree 3) kernel",
 )
+
+# Set-up 2x2 grid for plotting.
+fig, sub = plt.subplots(2, 2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+X0, X1 = X[:, 0], X[:, 1]
+
+for clf, title, ax in zip(models, titles, sub.flatten()):
+    disp = DecisionBoundaryDisplay.from_estimator(
+        clf,
+        X,
+        response_method="predict",
+        cmap=plt.cm.coolwarm,
+        alpha=0.8,
+        ax=ax,
+        xlabel='RestBP',
+        ylabel='Chol',
+    )
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors="k")
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(title)
+
 plt.show()
 
 
